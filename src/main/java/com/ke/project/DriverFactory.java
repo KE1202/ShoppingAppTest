ae4ff8e89f5363955f23cad25b73c60d8fb11f0c
KE1202 <gmjkenrique@gmail.com>
2019-11-17 00:29:47 -0800

    First commit

---
+++ src/main/java/com/ke/project/DriverFactory.java
@@ -0,0 +1,50 @@
+package com.ke.project;
+
+
+	
+
+	import java.io.File;
+	import java.net.MalformedURLException;
+	import java.net.URL;
+
+	import org.openqa.selenium.remote.DesiredCapabilities;
+
+	import io.appium.java_client.android.AndroidDriver;
+	import io.appium.java_client.android.AndroidElement;
+	import io.appium.java_client.remote.MobileCapabilityType;
+
+	public class DriverFactory {
+
+		public static AndroidDriver<AndroidElement> create(String deviceName, String autoName, boolean isRealDevice,
+				boolean isApk, String apkName) throws MalformedURLException {
+
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+
+			// isRealdevice X
+			// isApk Y
+			// IsEmulator Y
+
+			if (!isRealDevice || isApk) {
+				capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+			}
+
+			if (isApk) {
+				File f = new File("src");
+				File fs = new File(f, apkName);
+				capabilities.setCapability(MobileCapabilityType.APP, fs.getAbsolutePath());
+			} else {
+				capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "chrome");
+			}
+
+
+			AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+					new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			return driver;
+
+		}
+
+	}
+
+
+
---
+++ src/main/java/com/ke/project/ShoppingAppTest.java
@@ -0,0 +1,103 @@
+package com.ke.project;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+import static io.appium.java_client.touch.LongPressOptions.longPressOptions;
+import static java.time.Duration.ofSeconds;
+
+import java.io.File;
+
+public class ShoppingAppTest {
+
+	private static String pixel = "Pixel_API_28";
+	private static String nexus = "NEXUS";
+
+	private static String EMULATOR_NAME = "NEXUS";
+	private static String UI_AUTO_NAME = "uiautomator2";
+	private static boolean isRealDevice = false;
+	private static boolean isApk = true;
+	private static String PACKAGE_NAME = "com.bfa.android";
+	private static String SHOPPING_APK = "General-Store.apk";
+	private static String API_DEMOS = "ApiDemos-debug.apk";
+	private static String Browser = "Chrome";
+	private static AndroidDriver<AndroidElement> driver = null;
+
+	public static void main(String[] args) {
+		DriverFactory driverFactory = new DriverFactory();
+
+		try {
+			driver = driverFactory.create(EMULATOR_NAME, UI_AUTO_NAME, isRealDevice, isApk, SHOPPING_APK);
+			runApkTests(driver);
+
+		} catch (MalformedURLException k) {
+			System.out.println(" hey this path is wrong with error :" + k.getLocalizedMessage());
+			// } catch (InterruptedException e) {
+			// System.out.println(e.getLocalizedMessage());
+
+		}
+
+	}
+
+	private static void runApkTests(AndroidDriver<AndroidElement> driver) {
+		// test login success and failure (toastMessage)
+
+		testLogIn(driver);
+
+	}
+
+	private static void testLogIn(AndroidDriver<AndroidElement> driver) {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElementByXPath("//android.widget.TextView [@ text = 'Afghanistan']").click();
+		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Antarctica\"));");
+		driver.findElementByXPath("//android.widget.TextView [@text = 'Antarctica']").click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("FirstName and LastName");
+		driver.findElement(By.xpath("//*[@text='Female']")).click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+		AndroidElement backButton = driver.findElementById("com.androidsample.generalstore:id/appbar_btn_back");
+		if (backButton != null) {
+			backButton.click();
+		}
+
+		driver.findElementByXPath(
+				"/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RelativeLayout/"
+						+ "android.widget.FrameLayout/android.widget.LinearLayout/android.widget.EditText")
+				.clear();
+
+		// Log in failure
+
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+		String toastMessage = driver.findElement(By.xpath("//android.widget.Toast[1]")).getAttribute("name");
+
+		System.out.println(toastMessage);
+
+		Assert.assertEquals("Please enter your name", toastMessage);
+
+		try {
+			Thread.sleep(5000);
+			
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+
+		}
+		driver.quit();
+	}
+}
